f4bd2aa026b08e4c764fbeb7b3757908b90f2b04
Derek-Regier <sirderekoflane@gmail.com>
2026-02-12 14:40:30 -0700

    Add files via upload

---
+++ enemy.h
@@ -0,0 +1,16 @@
+typdef struct {
+    unsigned int x, y;
+    int delta_x;
+    int delta_y;
+    int health;
+    int damage;
+    unsigned int h,w;
+} Enemy;
+
+void move_enemy_vertical(Enemy *enemy);
+
+void move_enemy_horizontal(Enemy *enemy);
+
+bool update_enemy_health(Enemy *enemy, int x);
+
+void update_enemy_damage(Enemy *enemy, int x);

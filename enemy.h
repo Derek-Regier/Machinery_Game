3202fef545cab21f4e6481ff6565d547d1f4dda6
Derek-Regier <sirderekoflane@gmail.com>
2026-02-15 14:47:46 -0700

    Fix typedef declaration and add enemy_attack function

--- enemy.h
+++ enemy.h
@@ -1,4 +1,4 @@
-typdef struct {
+typedef struct {
     unsigned int x, y;
     int delta_x;
     int delta_y;
@@ -14,3 +14,5 @@ void move_enemy_horizontal(Enemy *enemy);
 bool update_enemy_health(Enemy *enemy, int x);
 
 void update_enemy_damage(Enemy *enemy, int x);
+
+int enemy_attack(Enemy *enemy);

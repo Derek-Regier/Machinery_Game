f850d1c4d54845cb65410df43700ba6685eaf072
Derek-Regier <sirderekoflane@gmail.com>
2026-02-15 14:48:23 -0700

    Fix typedef declaration and add light_attack function

--- player.h
+++ player.h
@@ -1,4 +1,4 @@
-typdef struct {
+typedef struct {
     unsigned int x, y;
     int delta_x;
     int delta_y;
@@ -14,3 +14,6 @@ void move_player_horizontal(Player *player);
 bool update_health(Player *player, int x);
 
 void update_damage(Player *player, int x);
+
+int light_attack(Player *player);
+

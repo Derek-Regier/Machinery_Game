ccc89e1958b12a44901a7aaa207d8926f2714b7d
Chintan Thakor <cthak393@mtroyal.ca>
2026-02-17 12:00:51 -0700

    added raster.c and raster.h, completed clear_screen function

---
+++ raster.c
@@ -0,0 +1,31 @@
+#include "raster.h"
+
+
+/* 
+Screen resolution 640 x 400 = 256,000 pixels
+                  256,000/8 = 32,000 bytes
+UINT32 = 4 bytes
+UINT16 = 2 bytes
+UINT8  = 1 byte
+
+*/
+
+
+void clear_screen(UINT32 *base){
+    
+    UINT32 i;
+    
+    // 32,000 bytes/4 bytes = 8000 32-bit words
+    for (i = 0; i < 8000; i++){         
+        base[i] = 0; 
+    }
+
+}
+
+void plot_pixel(UINT8 *base, UINT16 row, UINT16 col){
+
+}
+
+void plot_horizontal_line(UINT32 *base, UINT16 row, UINT16 col, UINT16 length){
+
+}

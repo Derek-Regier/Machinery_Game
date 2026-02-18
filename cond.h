e3892914bbd59d1149ff875d1ef8da33b86083f4
Derek-Regier <sirderekoflane@gmail.com>
2026-02-18 14:00:55 -0700

    Add player_dies function declaration in cond.h

--- cond.h
+++ cond.h
@@ -7,6 +7,7 @@ bool player_take_damage(Player *player, int damage);
 void update_health_HUD(Player *player);
 void boss_summon(Boss *boss);
 void player_hits_enemy(Model *model,int enemy_num);
-void enemy_hits_player(Model *model,int enemy_num);
+bool enemy_hits_player(Model *model,int enemy_num);
+void player_dies();
 bool level_end(Player *player);
 #endif

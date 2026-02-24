6f460df2d6d011dd6361b11d50a1d4ae82feece1
AydinS2124 <asalo379@mtroyal.ca>
2026-02-23 20:55:49 -0700

    defined render functions

---
+++ render.h
@@ -0,0 +1,6 @@
+void render(const Model *model, UINT8 * base);
+void render_player(const *player, UINT8 * base);
+void render_enemy(const *enemy, UINT8 * base);
+void render_boss(const *boss, UINT8 * base);
+void render_item(const *item, UINT8 * base);
+void render_health_bar(const *health_bar, UINT8 * base);

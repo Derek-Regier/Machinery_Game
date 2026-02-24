ded195ae74b1254539806d8535027514167e8b69
AydinS2124 <asalo379@mtroyal.ca>
2026-02-23 21:15:37 -0700

    added small bit of logic to function calls

--- render.c
+++ render.c
@@ -1,18 +1,50 @@
-void render(const Model *model, UINT8 * base){
+#include "render.h"
 
+void render(const Model *model, UINT8 *base){
+  
 }
-void render_player(const *player, UINT8 * base){
+void render_player(const *player, UINT8 *base){
 
+  UINT16 row;
+  UINT16 col;
+  UINT16 height;
+
+  //pbm??(*base, row, col, *player, height);
+  
 }
-void render_enemy(const *enemy, UINT8 * base){
+void render_enemy(const *enemy, UINT8 *base){
 
+  UINT16 row;
+  UINT16 col;
+  UINT16 height;
+
+  //pbm??(*base, row, col, *enemy, height);
+  
 }
-void render_boss(const *boss, UINT8 * base){
+void render_boss(const *boss, UINT8 *base){
 
+  UINT16 row;
+  UINT16 col;
+  UINT16 height;
+
+  //pbm??(*base, row, col, *boss, height);
+  
 }
-void render_item(const *item, UINT8 * base){
+void render_item(const *item, UINT8 *base){
 
+  UINT16 row;
+  UINT16 col;
+  UINT16 height;
+
+  //pbm??(*base, row, col, *item, height);
+  
 }
-void render_health_bar(const *health_bar, UINT8 * base){
+void render_health_bar(const *health_bar, UINT8 *base){
 
+  UINT16 row;
+  UINT16 col;
+  UINT16 height;
+
+  //pbm??(*base, row, col, *health_bar, height);
+  
 }

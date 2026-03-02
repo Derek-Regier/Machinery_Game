2fc771451e6a75a205bfe0fd11e4c274c24cd072
Derek-Regier <sirderekoflane@gmail.com>
2026-03-01 18:24:46 -0700

    Update item.h

--- item.h
+++ item.h
@@ -1,3 +1,11 @@
+/*
+* Healthbar object defintitions. Struct is there for position, value, and size only
+ * One function for retrieving value
+ *
+ * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
+ * Course: COMP 2659, Winter 2026
+ */
+
 #ifndef ITEM_H
 #define ITEM_H
 
@@ -7,7 +15,12 @@ typedef struct {
     int value;
 } Item;
 
+/* Function purpose: get the items value
+ * Input: The item object
+ * Output: An int of the items value
+ * Assumptions: None */
 int get_value(Item *item);
 
 #endif /* ITEM_H */
+
 

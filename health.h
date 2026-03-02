5df175ad2fb0a9683dc6594d76fa0f386a8d8eed
Derek-Regier <sirderekoflane@gmail.com>
2026-03-01 18:19:22 -0700

    Update health.h

--- health.h
+++ health.h
@@ -1,3 +1,11 @@
+/*
+ * Healthbar object defintitions. Struct is there for position, value, and size only
+ * One function for updating value
+ *
+ * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
+ * Course: COMP 2659, Winter 2026
+ */
+
 #ifndef HEALTH_H
 #define HEALTH_H
 
@@ -7,6 +15,11 @@ typedef struct {
     int value;
 } Healthbar;
 
+/* Function purpose: Update the healthbar object
+ * Input: The healthbar and the value to update
+ * Output: An int of the current helath value to be updated on screen
+ * Assumptions: correct values are passed */
 int update_value(Healthbar *healthbar, int value);
 
 #endif /* HEALTH_H */
+

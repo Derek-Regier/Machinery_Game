674ad4a0e97dfcd34007e42746a695451014d392
Derek-Regier <sirderekoflane@gmail.com>
2026-03-01 18:25:58 -0700

    Update item.c

--- item.c
+++ item.c
@@ -1,5 +1,17 @@
+/*
+ * Item function implementation
+ *
+ * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
+ * Course: COMP 2659, Winter 2026
+ */
+
 #include "item.h"
 
+/* Function purpose: get the items value
+ * Input: The item object
+ * Output: An int of the items value
+ * Assumptions: None */
 int get_value(Item *item){
     return item->value;
+
 }

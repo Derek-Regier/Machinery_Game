7cf28a8269ac0d87cf3a12caf1879fd58e441af6
Derek-Regier <sirderekoflane@gmail.com>
2026-03-01 18:47:06 -0700

    Update synch.h

--- synch.h
+++ synch.h
@@ -1,4 +1,10 @@
-/* synch.h */
+/*
+ * Synch events defnitition
+ * All synchronous event handlers are defined here for updating movement and cooldowns
+ *
+ * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
+ * Course: COMP 2659, Winter 2026
+ */
 #ifndef SYNCH_H
 #define SYNCH_H
 
@@ -7,16 +13,44 @@
 #include "boss.h"
 #include "model.h"
 
-/* Core Movement Updates */
+
+/* Function purpose: Moves the player according to the velocity
+ * Input: The player object
+ * Output: None, moves the player object position on the screen
+ * Assumptions: The player has an appropriate velocity */
 void update_player_position(Player *player);
+
+/* Function purpose: Moves the enemy according to the velocity
+ * Input: The enemy object
+ * Output: None, moves the enemy object position on the screen
+ * Assumptions: The enemy has an appropriate velocity */
 void update_enemy_position(Enemy *enemy, const Player *player);
+
+/* Function purpose: Moves the boss according to the velocity
+ * Input: The boss object
+ * Output: None, moves the boss object position on the screen
+ * Assumptions: The boss has an appropriate velocity */
 void update_boss_position(Boss *boss, const Player *player);
 
-/* Cooldown Management */
+/* Function purpose: updates the player cooldowns together
+ * Input: The player object
+ * Output: None, just updates the cooldowns
+ * Assumptions: The other cooldown functions are working properly */
 void update_player_cooldowns(Player *player);
+
+/* Function purpose: Updates the player attack cooldown
+ * Input: The player object
+ * Output: None, decrements the cooldown, and resets is attacking to false if cooldown is <=0
+ * Assumptions: The function is called at the appropriate timings */
 void update_attack_cooldown(Player *player);
+
+/* Function purpose: Updates the player item cooldown
+ * Input: The player object
+ * Output: None, decrements the cooldown
+ * Assumptions: The function is called at the appropriate timings */
 void update_item_cooldown(Player *player);
 
 /* Animation functions TODO */
 
 #endif /* SYNCH_H */
+

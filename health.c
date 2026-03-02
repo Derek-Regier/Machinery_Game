50775ebe8db6bc0a679f72b74d7699fb9852dea5
Derek-Regier <sirderekoflane@gmail.com>
2026-03-01 18:20:17 -0700

    Update health.c

--- health.c
+++ health.c
@@ -1,6 +1,18 @@
+/*
+ * Healthbar function implementation
+ *
+ * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
+ * Course: COMP 2659, Winter 2026
+ */
+
 #include "health.h"
 
+/* Function purpose: Update the healthbar object
+ * Input: The healthbar and the value to update
+ * Output: An int of the current helath value to be updated on screen
+ * Assumptions: correct values are passed */
 int update_value(Healthbar *healthbar, int value){
     healthbar->value += value;
     return healthbar->value;
 }
+

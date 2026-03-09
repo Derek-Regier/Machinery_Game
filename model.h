a7ab8397cc8961b49a9679348e1c270616293e23
Derek-Regier <sirderekoflane@gmail.com>
2026-03-08 19:32:00 -0600

    Update model.h

--- model.h
+++ model.h
@@ -12,12 +12,14 @@
 #include "enemy.h"
 #include "boss.h"
 #include "item.h"
+#include "types.h"
 
 typedef struct {
     Player player;
     Enemy enemy[1]; /*Placeholder, actual enemies will depend on gameplay*/
     Boss boss;
     Item item[1]; /*Number of items will also depend on gameplay*/
+    bool quit;
 } Model;
 
 /*
@@ -30,5 +32,6 @@ typedef struct {
 void init_model(Model *model);
 
 #endif
+
 
 

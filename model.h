2716e358abe79c4997a58d109f68207e1bc69d85
Derek-Regier <sirderekoflane@gmail.com>
2026-03-12 14:49:20 -0600

    Added in Item array and maximum

--- model.h
+++ model.h
@@ -15,15 +15,17 @@
 #include "types.h"
 #include "health.h"
 
-#define MAX_ENEMIES 10
+#define MAX_ENEMIES 16  /* 2 tutorial + 3 + 4 + 5 wave + 2 boss summon */
+#define NUM_ITEMS   4   /* one item drop per wave (stages 0-3) */
 
 typedef struct {
     Player player;
     Healthbar healthbar;
-    Enemy enemy[MAX_ENEMIES];  /* define MAX_ENEMIES as a constant */
-    int enemy_count; 
+    Enemy enemy[MAX_ENEMIES];
+    int enemy_count;
     Boss boss;
-    Item item[1]; /*Number of items will also depend on gameplay*/
+    Item item[NUM_ITEMS];   /* item[stage] is dropped when that wave clears */
+    int stage;              /* current wave: 0=tutorial, 1-3=waves, 4=boss */
     bool quit;
 } Model;
 
@@ -38,6 +40,3 @@ void init_model(Model *model);
 void reset_movement(Model *model);
 
 #endif
-
-
-

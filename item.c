dbf3997806f3a84ff8083ad424d88d4f54515168
Derek Regier <dregi906@mtroyal.ca>
2026-03-13 16:44:04 -0600

    Fixed conflict issue

--- item.c
+++ item.c
@@ -6,6 +6,7 @@
  */
 
 #include "item.h"
+#include "types.h"
 
 /* Function purpose: get the items value
  * Input: The item object
@@ -13,5 +14,12 @@
  * Assumptions: None */
 int get_value(Item *item){
     return item->value;
+
+}
+
+bool item_hitbox_overlaps(Item *item, int x, int y, int width, int height){
+    
+    return (item->x == x &&       /* Enemy's right edge is right of player's left edge */
+            item->y == y);        /* Enemy's bottom edge is below player's top edge */
 
 }

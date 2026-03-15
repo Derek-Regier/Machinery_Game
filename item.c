ac917ebde6257a6bf4f91bf87e9d85974f4bcca6
Derek-Regier <sirderekoflane@gmail.com>
2026-03-15 17:21:36 -0600

    Update item.c

--- item.c
+++ item.c
@@ -4,22 +4,22 @@
  * Authors: Aydin Salonius, Chintan Thakor, Derek Regier
  * Course: COMP 2659, Winter 2026
  */
-
+ 
 #include "item.h"
 #include "types.h"
-
+ 
 /* Function purpose: get the items value
  * Input: The item object
  * Output: An int of the items value
  * Assumptions: None */
 int get_value(Item *item){
     return item->value;
-
+ 
 }
-
-bool item_hitbox_overlaps(Item *item, int x, int y, int width, int height){
-    
-    return (item->x == x &&       /* Enemy's right edge is right of player's left edge */
-            item->y == y);        /* Enemy's bottom edge is below player's top edge */
-
+ 
+bool item_hitbox_overlaps(const Item *item, int x, int y, int width, int height){
+    return (item->x < (unsigned int)(x + width)  &&
+            item->x + item->w > (unsigned int)x  &&
+            item->y < (unsigned int)(y + height) &&
+            item->y + item->h > (unsigned int)y);
 }

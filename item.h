4142494a1a42551471a84ed26c7b4dbb5c0aa966
Derek-Regier <sirderekoflane@gmail.com>
2026-03-16 20:35:05 -0600

    Update docu,tation

--- item.h
+++ item.h
@@ -27,6 +27,10 @@ typedef struct {
  * Assumptions: None */
 int get_value(Item *item);
 
+/* Function purpose: Determine if an the player overlaps with the item 
+ * Input: The item object, the x position of the other object, the y position, the width, and the height
+ * Output: Bool -> if both x and y overlap with the boss considering height and width, return true
+ * Assumptions: Valid screen positions and model height and width are passed */
 bool item_hitbox_overlaps(const Item *item, int x, int y, int width, int height);
 
 #endif /* ITEM_H */

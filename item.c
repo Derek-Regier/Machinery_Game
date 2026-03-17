01713b435b5bd06a334a818a7e8277fe7efa6c7d
Derek-Regier <sirderekoflane@gmail.com>
2026-03-16 20:34:39 -0600

    Added header

--- item.c
+++ item.c
@@ -17,6 +17,10 @@ int get_value(Item *item){
  
 }
  
+/* Function purpose: Determine if an the player overlaps with the item 
+ * Input: The item object, the x position of the other object, the y position, the width, and the height
+ * Output: Bool -> if both x and y overlap with the boss considering height and width, return true
+ * Assumptions: Valid screen positions and model height and width are passed */
 bool item_hitbox_overlaps(const Item *item, int x, int y, int width, int height){
     return (item->x < (unsigned int)(x + width)  &&
             item->x + item->w > (unsigned int)x  &&
